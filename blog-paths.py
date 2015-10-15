82b77f0e1ec11a2d2fdc3b375a1ee864f8e5169f
CookieCutter <mingrui.li3@gmail.com>
2015-10-15 03:31:28 -0400

    log in function

--- blog-paths.py
+++ blog-paths.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+dict = {"test":123}
+
+@app.route("/", methods = ["POST"])
 def login():
-    return render_template("login.html")
+    if str(request.form["submitting"]) == "Login_In":
+        print("1");
+        username = str(request.form["username"])
+        if username in dict.keys():
+            print("3")
+            print(dict[username])
+            print(request.form["password"])
+            if str(dict[username]) == str(request.form["password"]):
+                return render_template("/feed.html")
+            else:
+                return render_template("/login.html", text = "Username/Password does not match")
+        else:
+            return render_template("/login.html", text = "Username does not exist")
+    else:
+        print("2")
+        return render_template("/login.html")
 
 @app.route("/register")
 def register():
