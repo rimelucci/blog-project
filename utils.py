7fa7d6e0ba48fdba68d21d71874a3b1a53a519d4
M4598BM <milorobin@gmail.com>
2015-10-17 16:44:29 -0400

    Fixed the thread issue

--- utils.py
+++ utils.py
@@ -1,19 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect("Data.db")
-c = conn.cursor()
-
 def pwordAuth(uname, pword):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     p = c.execute("SELECT pword FROM accounts WHERE uname = '"+uname+"';")
     for r in p:
         return r[0] == pword
     return False
 
 def addAccount(uname, pword):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
+    accounts = c.execute("SELECT uname FROM accounts")
+    for r in accounts:
+        if r[0] == uname:
+            return "This account name already exists"
     c.execute("INSERT INTO accounts VALUES (?, ?);", (uname, pword))
     conn.commit()
 
 def changePword(uname, oldP, newP, cNewP):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     p = c.execute("SELECT pword FROM accounts WHERE uname = '"+uname+"';")
     for r in p:
         result = r[0]
@@ -38,12 +45,16 @@ def changePword(uname, oldP, newP, cNewP):
 # (id, uname)
 
 def setup():
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     c.execute("CREATE TABLE posts (id integer, uname text, post text, time text)")
     c.execute("CREATE TABLE comments (id integer, uname text, comment text, time text)")
     c.execute("CREATE TABLE posts (id integer, uname text)")
     conn.commit()
 
 def findID():
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     ids = c.execute("SELECT id FROM posts")
     max = 0
     for r in ids:
@@ -53,20 +64,27 @@ def findID():
     return max+1
 
 def addPost(uname, post):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     c.execute("INSERT INTO posts VALUES (?, ?, ?, ?);", (findID(), uname, post, displayDate()))
     conn.commit()
 
 def showPost(ID):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     post = c.execute("SELECT post FROM posts WHERE id = "+str(ID)+";")
     for r in post:
         return r[0]
 def showPosts(uname):
+    conn = sqlite3.connect("Data.db")
+    c = conn.cursor()
     posts = c.execute("SELECT post FROM posts WHERE uname = '"+uname+"';")
     list = []
     for r in posts:
         list.append(r[0])
     return list
-print(showPosts("Milo"))
+
+
 def displayDate():
     import datetime
     return str(datetime.datetime.now())
