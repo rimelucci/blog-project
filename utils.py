60eefbb7c00c42ae8dcf92d6d83adf0d4088461d
M4598BM <milorobin@gmail.com>
2015-10-17 13:22:53 -0400

    Started working on posts

--- utils.py
+++ utils.py
@@ -38,7 +38,48 @@ def changePword(uname, oldP, newP, cNewP):
 # (id, uname)
 
 def setup():
-    c.execute("CREATE TABLE posts (id integer, uname text, post text, time timestamp)")
-    c.execute("CREATE TABLE comments (id integer, uname text, comment text, time timestamp)")
+    c.execute("CREATE TABLE posts (id integer, uname text, post text, time text)")
+    c.execute("CREATE TABLE comments (id integer, uname text, comment text, time text)")
     c.execute("CREATE TABLE posts (id integer, uname text)")
     conn.commit()
+
+def findID():
+    ids = c.execute("SELECT id FROM posts")
+    max = 0
+    for r in ids:
+        id = r[0]
+        if (id > max):
+            max = id
+    return max+1
+
+def addPost(uname, post):
+    c.execute("INSERT INTO posts VALUES (?, ?, ?, ?);", (findID(), uname, post, displayDate()))
+    conn.commit()
+
+def showPost(ID):
+    post = c.execute("SELECT post FROM posts WHERE id = "+str(ID)+";")
+    for r in post:
+        return r[0]
+def showPosts(uname):
+    posts = c.execute("SELECT post FROM posts WHERE uname = '"+uname+"';")
+    list = []
+    for r in posts:
+        list.append(r[0])
+    return list
+print(showPosts("Milo"))
+def displayDate():
+    import datetime
+    return str(datetime.datetime.now())
+
+def printDate(d):
+    d = d.split(" ")
+    date = ""
+
+    time = d[1].split(":")
+    day = d[0].split("-")
+    
+    date += day[1]+"/"+day[2]+"/"+day[0]+" at "
+    date += time[0]+":"+time[1]
+    return date
+ 
+print(printDate(displayDate()))
